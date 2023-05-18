9d1be1225e63c091008462bec45fe5539483103c
Jay200123 <kenken.ayonon@gmail.com>
2023-05-18 13:57:25 +0800

    users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 const asyncHandler = require('express-async-handler')
 
 exports.allUsers = asyncHandler(async(req, res)=>{
@@ -40,6 +41,41 @@ exports.getOneUser = asyncHandler(async(req, res)=>{
 
     res.status(200).json({
         success:true,
+        users
+    })
+})
+
+exports.updateUsers = asyncHandler(async(req, res)=>{
+
+    const { id }  = req.params
+    const { password }  = req.body
+
+    if(password){
+        req.body.password = await bcrypt.hash(password,10) //keeps the password hash when updating the user's data
+    }
+
+    const users = await User.findByIdAndUpdate(id, req.body)
+
+    if(!users){
+        res.status(500).json({message:"Error cant update data"})
+    }
+    res.status(200).json({
+        sucess:true,
+        users
+    })
+})
+
+exports.deleteUser = asyncHandler(async(req, res)=>{
+
+    const { id } = req.params
+    const users = User.findByIdAndDelete(id)
+
+    if(!users){
+        res.status(500).json({ message:"Error cant remove the data"})
+    }
+
+    res.status(200).json({
+        sucess:true,
         users
     })
 })
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const router = express.Router()
 
-const { allUsers, storeUsers, getOneUser } = require('../controllers/userController')
+const { allUsers, storeUsers, getOneUser, updateUsers, deleteUser } = require('../controllers/userController')
 
 router.get('/users', allUsers)
 router.post('/users/store', storeUsers)
 router.get('/users/:id', getOneUser)
+router.put('/users/update/:id', updateUsers)
+router.delete('/users/delete/:id', deleteUser)
 
 module.exports = router
