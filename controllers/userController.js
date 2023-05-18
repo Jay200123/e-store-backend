d3e0f0445a521215632ed0a2bfc9a0dd787faa82
Jay200123 <kenken.ayonon@gmail.com>
2023-05-18 13:46:17 +0800

    users

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,32 @@ exports.allUsers = asyncHandler(async(req, res)=>{
         users
     })
 })
+
+exports.storeUsers = asyncHandler(async(req, res)=>{
+
+    const users = await User.create(req.body)
+
+    if(!users){
+        res.status(500).json({message: "Error Field Required"})
+    }
+
+    res.status(200).json({
+        success:true,
+        users
+    })
+})
+
+exports.getOneUser = asyncHandler(async(req, res)=>{
+
+    const { id } = req.params
+    const users = await User.findById(id)
+
+    if(!users){
+        res.status(404).json({ message:"Data not Found"})
+    }
+
+    res.status(200).json({
+        success:true,
+        users
+    })
+})
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const { allUsers } = require('../controllers/userController')
+const { allUsers, storeUsers, getOneUser } = require('../controllers/userController')
 
 router.get('/users', allUsers)
-
+router.post('/users/store', storeUsers)
+router.get('/users/:id', getOneUser)
 
 module.exports = router
