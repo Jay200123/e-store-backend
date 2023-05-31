6ca13ab456b01b839b36ec358e2c2d0d0376c88a
Jay200123 <kenken.ayonon@gmail.com>
2023-05-31 21:10:40 +0800

    send email

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,4 +1,6 @@
 const nodemailer = require('nodemailer')
+const fs = require('fs')
+const path = require('path')
 
 const sendEmail = async(options)=>{
 
@@ -12,13 +14,15 @@ const sendEmail = async(options)=>{
         }
     })
 
+    const body = fs.readFileSync(path.join(__dirname, '../sendMail/message.html'))
+
     const msg = {
 
         from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
         to: options.email,
         subject: options.subject,
         text: options.text || `Thank You For Registering to E-Store Web Application`,
-        html: options.html,
+        html: body,
     }
 
     await transport.sendMail(msg)
